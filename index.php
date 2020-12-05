db84c133f422274b2660691a54f899a200971bfc
Nitesh Sharma <rs.nitesh01@gmail.com>
2020-12-05 23:43:51 +0530

    initial commit...

---
+++ index.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Scholarship Assistant</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="owl.carousel.min.css"/>
+    <script src="script.js"></script>
+
+</head>
+<body>
+<div class="scroll-up-btn">
+    <i class="fas fa-angle-up"></i>
+</div>
+<nav class="navbar">
+    <div class="max-width">
+        <div class="logo"><a href="#"><img src="images/logo.png"></a></div>
+        <ul class="menu">
+            <li><a href="#home" class="menu-btn">Home</a></li>
+            <li><a href="#about" class="menu-btn">About</a></li>
+            <li><a href="#services" class="menu-btn">Services</a></li>
+            <li><a href="#Schemes" class="menu-btn">Schemes</a></li>
+            <li><a href="#teams" class="menu-btn">Teams</a></li>
+            <li><a href="#contact" class="menu-btn">Contact</a></li>
+        </ul>
+        <div class="menu-btn">
+            <i class="fas fa-bars"></i>
+        </div>
+    </div>
+</nav>
+
+<!-- home section start -->
+<section class="home" id="home">
+    <div class="max-width">
+        <div class="home-content">
+            <div class="text-1">Hello, Students</div>
+            <div class="text-2">Schemes Assistant</div>
+            <div class="text-3">We Assist you for <span class="typing"></span></div>
+            <a href="#">Hire me</a>
+        </div>
+    </div>
+</section>
+
+<!-- about section start -->
+<section class="about" id="about">
+    <div class="max-width">
+        <h2 class="title">About</h2>
+        <div class="about-content">
+            <div class="column left">
+                <img src="images/logo.png">
+            </div>
+            <div class="column right">
+                <div class="text">We Aware, Help and Assist you for  <span class="typing-2"></span></div>
+                <p>We are always help for providing Government schemes benefits to the people. Numbers of schemes are being implemented by Central Government and States/UT Government for socio-economic development. Name of the schemes implemented amount of financial assistance given under the schemes and conditions laid down therein which are to be completed by the applicant, Schemes name and Department of the States/UTs dealing with the various schemes is given in the last. </p>
+                <a href="#">Guidelines</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- services section start -->
+<section class="services" id="services">
+    <div class="max-width">
+        <h2 class="title">Scholarship Schemes</h2>
+        <div class="serv-content">
+            <div class="card">
+                <div class="box">
+                    <i class="fab fa-accusoft"></i>
+                    <div class="text"></div>
+                    <p>Reimbursement of tuition fees to the SC/ST/OBC/Minority students of Class I to XII, studying in recognized public/private schools of Delhi. </p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <i class="fab fa-accusoft"></i>
+                    <div class="text"></div>
+                    <p>Scholarship scheme for SC student for pursuing Higher Studies in Abroad.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <i class="fab fa-accusoft"></i>
+                    <div class="text"></div>
+                    <p>Mukhyamantri Vidyarthi Pratibha Yojna for SC/ST/OBC/Minority Students</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <i class="fab fa-accusoft"></i>
+                    <div class="text"></div>
+                    <p>Merit scholarship for students belonging to SC/ST/OBC/Minority category studying in Professional/ technical Colleges/ Institutions/ Universities.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <i class="fab fa-accusoft"></i>
+                    <div class="text"></div>
+                    <p>Dr. B. R. Ambedkar State Award for Toppers amongst SC/ST/OBC/MIN Category Students.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <i class="fab fa-accusoft"></i>
+                    <div class="text"></div>
+                    <p>Post-Matric Scholarship for SC Students.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <i class="fab fa-accusoft"></i>
+                    <div class="text"></div>
+                    <p>Pre-Matric Scholarship for SC Students.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <i class="fab fa-accusoft"></i>
+                    <div class="text"></div>
+                    <p>Post-Matric Scholarship for OBC Students.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <i class="fab fa-accusoft"></i>
+                    <div class="text"></div>
+                    <p> Pre- Matric Scholarship for OBC  Students.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+<!-- Schemes section start -->
+<section class="Schemes" id="Schemes">
+    <div class="max-width">
+        <h2 class="title">Scholarship Schemes</h2>
+        <div class="Schemes-content">
+            <div class="column left">
+                <div class="text">We’ve supported students for over India.</div>
+                <p>The Students easy to help through this portal that Ensure timely apply of Scholarships to students provide a common portal for various Scholarships schemes of Central and State Governments. We guide to you according to Government schemes “Scheme aims at recognizing and promoting quality education amongst students, by providing financial support. The scheme will cover students for pursuing studies.</p>
+                <a href="#">Find A Scholarship</a>
+            </div>
+            <div class="column right">
+                <div class="bars">
+                    <div class="info">
+                        <span>Reimbursement of Tuition Fees</span>
+                        <span>90%</span>
+                    </div>
+                    <div class="line html"></div>
+                </div>
+                <div class="bars">
+                    <div class="info">
+                        <span>Higher Studies in Abroad for SC</span>
+                        <span>60%</span>
+                    </div>
+                    <div class="line css"></div>
+                </div>
+                <div class="bars">
+                    <div class="info">
+                        <span>Mukhyamantri Vidyarthi Pratibha Yojna</span>
+                        <span>80%</span>
+                    </div>
+                    <div class="line js"></div>
+                </div>
+                <div class="bars">
+                    <div class="info">
+                        <span>Merit scholarship College</span>
+                        <span>50%</span>
+                    </div>
+                    <div class="line php"></div>
+                </div>
+                <div class="bars">
+                    <div class="info">
+                        <span>Post Matric Scholarship</span>
+                        <span>70%</span>
+                    </div>
+                    <div class="line mysql"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- teams section start -->
+<section class="teams" id="teams">
+    <div class="max-width">
+        <h2 class="title">My teams</h2>
+        <div class="carousel owl-carousel">
+            <div class="card">
+                <div class="box">
+                    <img src="images/logo.png" alt="">
+                    <div class="text">Someone name</div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <img src="images/profile-2.jpeg" alt="">
+                    <div class="text">Someone name</div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <img src="images/profile-3.jpeg" alt="">
+                    <div class="text">Someone name</div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <img src="images/profile-4.jpeg" alt="">
+                    <div class="text">Someone name</div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <img src="images/profile-5.jpeg" alt="">
+                    <div class="text">Someone name</div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- contact section start -->
+<section class="contact" id="contact">
+    <div class="max-width">
+        <h2 class="title">Contact me</h2>
+        <div class="contact-content">
+            <div class="column left">
+                <div class="text">Dear Students</div>
+                <p>Scholarship Assistant Service is here to help you break down barriers, open doors and access scholarships that help you succeed in life.</p>
+                <div class="icons">
+                    <div class="row">
+                        <i class="fas fa-user"></i>
+                        <div class="info">
+                            <div class="head">Name</div>
+                            <div class="sub-title">Schemes Assistant</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <i class="fas fa-map-marker-alt"></i>
+                        <div class="info">
+                            <div class="head">Address</div>
+                            <div class="sub-title">In Your Heart and Mind</div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <i class="fas fa-envelope"></i>
+                        <div class="info">
+                            <div class="head">Email</div>
+                            <div class="sub-title">abc@gmail.com</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column right">
+                <div class="text">Message me</div>
+                <form action="#">
+                    <div class="fields">
+                        <div class="field name">
+                            <input type="text" placeholder="Name" required>
+                        </div>
+                        <div class="field email">
+                            <input type="email" placeholder="Email" required>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <input type="text" placeholder="Subject" required>
+                    </div>
+                    <div class="field textarea">
+                        <textarea cols="30" rows="10" placeholder="Message.." required></textarea>
+                    </div>
+                    <div class="button">
+                        <button type="submit">Send message</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- footer section start -->
+<footer>
+    <span>Created By <a href="#">Scholarship Assistant</a> | <span class="far fa-copyright"></span> 2020 All rights reserved.</span>
+</footer>
+
+<script src="script.js"></script>
+</body>
+</html>
